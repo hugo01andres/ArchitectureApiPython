6089427a39ba9df2900fb597960e2b6a02343c2f
hugo01andres <hugoandres01@live.com.mx>
2023-09-18 10:49:44 -0600

    Funciona la conexion, no de la mejor manera

--- app/app.py
+++ app/app.py
@@ -2,8 +2,16 @@ from flask import Flask
 from app.Infraestructure.db import db
 from app.Presentation.UsuarioController import usuario_bp
 from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
 def create_app():
     app = Flask(__name__)
+    print(os.getenv('DATABASE_URL'))
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
     # Registra los blueprints
